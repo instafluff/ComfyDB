c343d9249fcd0098d2017b20e371298368663ed1
Instafluff <35773245+instafluff@users.noreply.github.com>
2019-06-27 12:50:08 -0700

    Core Elements Complete

--- example.js
+++ example.js
@@ -10,10 +10,22 @@ async function testComfy() {
     var collections = await ComfyDB.Collections.List();
     console.log( collections );
     await ComfyDB.Data.SetByKey( "test", "a", { name: "test" }, true );
-    await ComfyDB.Data.SetByKey( "test", [ "a", "b", "c" ], [ { name: "A" }, { name: "B" }, { name: "C" } ], true );
+    var item = await ComfyDB.Data.FindByKey( "test", "a" );
+    console.log( item );
+    await new Promise((res) => setTimeout(res, 1000));
+    await ComfyDB.Data.SetByKey( "test", [ "a", "b", "c" ], [ { name: "A", points: 5 }, { name: "B" }, { name: "C" } ], true );
     await ComfyDB.Data.SetByKey( "test", "a", { name: "Z" }, true );
-    var all = await ComfyDB.Data.Find( "test", {} );
-    console.log( all );
+    var all = await ComfyDB.Data.FindLatest( "test" );//await ComfyDB.Data.Find( "test", {} );
+    console.table( all );
+    await ComfyDB.Data.Increment( "test", [ "a", "b" ], "points", 10 );
+    var itemA = await ComfyDB.Data.FindByField( "test", "name", ComfyDB.Is.NotEqual, "C" );
+    console.table( itemA );
+    await ComfyDB.Data.Decrement( "test", [ "a", "b" ], "points", 3 );
+    var itemA = await ComfyDB.Data.FindByField( "test", "name", ComfyDB.Is.NotEqual, "C" );
+    console.table( itemA );
+    await ComfyDB.Data.DeleteByKey( "test", "b" );
+    all = await ComfyDB.Data.FindLatest( "test" );//await ComfyDB.Data.Find( "test", {} );
+    console.table( all );
     console.log( "deleting collection..." );
     await ComfyDB.Collections.Delete( "test" );
     console.log( "listing collections..." );
@@ -26,7 +38,7 @@ async function testComfy() {
   finally {
     console.log( "closing..." );
     ComfyDB.Close();
-    process.kill( process.pid, "SIGTERM" );
+    // process.kill( process.pid, "SIGTERM" );
   }
 }
 
--- index.js
+++ index.js
@@ -50,27 +50,28 @@ const COMPARE = {
 };
 
 let comfyDB = {
+  _mongo: null,
   _client: null,
   _DB: null,
   Init: function( { url = "mongodb://localhost:27017", name = "comfyDB" } = {} ) {
     return new Promise( async ( resolve, reject ) => {
       try {
         // TODO: If we're not depending on the default local instance of mongo, skip booting it up
-        const mongo = require( "comfy-mongo" )();
-        mongo.on( "output", ( data ) => {
+        comfyDB._mongo = require( "comfy-mongo" )();
+        comfyDB._mongo.on( "output", ( data ) => {
           // console.log( data );
         });
-        mongo.on( "error", ( err ) => {
+        comfyDB._mongo.on( "error", ( err ) => {
           // console.log( err );
           reject( err );
         });
-        mongo.on( "ready", async () => {
+        comfyDB._mongo.on( "ready", async () => {
           console.log( "Ready..." );
           comfyDB._client = await MongoClient.connect( url, { useNewUrlParser: true } );
           comfyDB._DB = comfyDB._client.db( name );
           resolve();
         });
-        mongo.on( "exit", ( code ) => {
+        comfyDB._mongo.on( "exit", ( code ) => {
           console.log( "Exit:", code );
         });
       }
@@ -84,6 +85,9 @@ let comfyDB = {
       comfyDB._client.close();
       comfyDB._client = null;
       comfyDB._DB = null;
+    }
+    if( comfyDB._mongo ) {
+      comfyDB._mongo.shutdown();
     }
   },
   Backup: function() {
@@ -114,7 +118,7 @@ let comfyDB = {
     },
     SetByKey: function( collection, key, data, overwrite = true ) {
       // check if key is a single string or an array for batch update
-      // TODO: Add Objects, tagging with ID and timestamp for create/update
+      // TODO: Add timestamp for create/update
       const set = comfyDB._DB.collection( collection );
       let values = [];
       if( typeof key === "string" || key instanceof String ) {
@@ -130,36 +134,135 @@ let comfyDB = {
       else {
         throw new Error( "Invalid Argument Type" );
       }
+      values = values.map( x => ({ ...x, updatedAt: new Date() } ) );
 
       if( overwrite ) {
         let bulkOp = set.initializeUnorderedBulkOp();
-        key.forEach( ( k, i ) => bulkOp.find( { key: k } ).upsert().updateOne( { $set: values[ i ] }, { upsert: true } ) );
+        key.forEach( ( k, i ) => bulkOp.find( { key: k } ).upsert().updateOne( {
+          $set: values[ i ],
+          $setOnInsert: {
+            createdAt: new Date()
+          }
+        }, { upsert: true } ) );
         return bulkOp.execute();
       }
       else {
         return set.insertMany( values );
       }
     },
-    Delete: function( collection, key ) {
+    Delete: function( collection, { options } ) {
       // check if key is a single string or an array for batch update
       // TODO: Delete objects
     },
-    Increment: function( collection, key, field, amount ) {},
+    DeleteByKey: function( collection, key ) {
+      // check if key is a single string or an array for batch update
+      const set = comfyDB._DB.collection( collection );
+      if( typeof key === "string" || key instanceof String ) {
+        key = [ key ];
+      }
+      else if( Array.isArray( key ) ) {
+      }
+      else {
+        throw new Error( "Invalid Argument Type" );
+      }
+
+      let bulkOp = set.initializeUnorderedBulkOp();
+      key.forEach( ( k, i ) => bulkOp.find( { key: k } ).remove() );
+      return bulkOp.execute();
+    },
+    Increment: function( collection, key, field, amount ) {
+      const set = comfyDB._DB.collection( collection );
+      let values = [];
+      if( typeof key === "string" || key instanceof String ) {
+        values = [ { key: key } ];
+        key = [ key ];
+      }
+      else if( Array.isArray( key ) ) {
+        values = key.map( ( x, i ) => ({ key: x }) );
+      }
+      else {
+        throw new Error( "Invalid Argument Type" );
+      }
+      values = values.map( x => ({ ...x, updatedAt: new Date() } ) );
+
+      let bulkOp = set.initializeUnorderedBulkOp();
+      key.forEach( ( k, i ) => bulkOp.find( { key: k } ).upsert().updateOne( {
+        $set: values[ i ],
+        $inc: { [field]: amount },
+        $setOnInsert: {
+          createdAt: new Date()
+        }
+      }, { upsert: true } ) );
+      return bulkOp.execute();
+    },
     Decrement: function( collection, key, field, amount ) {
-      comfyDB.Increment( collection, key, field, -amount );
+      comfyDB.Data.Increment( collection, key, field, -amount );
     },
     Find: function( collection, options ) {
+      // TODO: Add time query for how long it took
       const set = comfyDB._DB.collection( collection );
-      return set.find().toArray();
+      let search = {};
+      let sort = null;
+      if( options.id ) {
+        search[ "_id" ] = options.id;
+      }
+      if( options.key ) {
+        search[ "key" ] = options.key;
+      }
+      if( options.sortBy ) {
+        if( typeof options.isOrderDescending !== undefined ) {
+          sort = { [options.sortBy]: options.isOrderDescending ? -1 : 1 };
+        }
+      }
+      if( options.field ) {
+        switch( options.compare ) {
+          case COMPARE.Equal:
+            search[ options.field ] = { $eq: options.value };
+            break;
+          case COMPARE.NotEqual:
+            search[ options.field ] = { $ne: options.value };
+            break;
+          case COMPARE.LessThan:
+            search[ options.field ] = { $lt: options.value };
+            break;
+          case COMPARE.LessThanOrEqual:
+            search[ options.field ] = { $lte: options.value };
+            break;
+          case COMPARE.GreaterThan:
+            search[ options.field ] = { $gt: options.value };
+            break;
+          case COMPARE.GreaterThanOrEqual:
+            search[ options.field ] = { $gte: options.value };
+            break;
+            // TODO: Add the remaining comparisons!
+            // StartsWith: "^",
+            // EndsWith: "$",
+            // Contains: "_",
+            // True: "1",
+            // False: "0"
+        }
+      }
+
+      console.log( search );
+
+      let query = set.find( search );
+      if( options.count ) {
+        query = query.limit( options.count );
+      }
+      if( sort ) {
+        query = query.sort( sort );
+      }
+      return query.toArray();
     },
     FindById: function( collection, id ) {
-      comfyDB.Find( { id } );
+      return comfyDB.Data.Find( collection, { id } );
     },
     FindByKey: function( collection, key ) {
-      comfyDB.Find( { key } );
+      return comfyDB.Data.Find( collection, { key } );
     },
     FindByField: function( collection, field, compare = COMPARE.True, value = "", count = 100, descending = true ) {
-      comfyDB.Find( {
+      // TODO: Support an array of fields and comparisons
+      return comfyDB.Data.Find( collection, {
         field,
         compare,
         value,
@@ -168,13 +271,16 @@ let comfyDB = {
       });
     },
     FindLatest: function( collection, count = 100 ) {
-      comfyDB.Find( {
-        key: "updated",
+      return comfyDB.Data.Find( collection, {
+        sortBy: "updatedAt",
+        isOrderDescending: true,
         count,
-        isOrderDescending: true
       } );
     },
-    Count: function( collection, key, compare = COMPARE.True, value = "" ) {},
+    Count: function( collection, field, compare = COMPARE.True, value = "" ) {
+      // TODO: Optimize!!!
+      return comfyDB.Data.FindByField( collection, field, compare, value ).length;
+    },
   },
 };
 
