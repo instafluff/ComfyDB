f6c73e23ba5bae0d1dbe2513118c033c34d638f3
Jacob Foster <jfost3@gmail.com>
2019-10-17 20:18:02 -0500

    Don't load assert if it's not being used

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// const assert = require('assert');
 
 // // Connection URL
 // const url = 'mongodb://localhost:27017';
