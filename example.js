6e846bbf866bf21345f7c385e2a55c5092d44de0
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-03-31 15:01:01 -0700

    v2 Initial Version

--- example.js
+++ example.js
@@ -2,35 +2,25 @@ const ComfyDB = require( "./index" );
 
 async function testComfy() {
   try {
-    console.log( "initializing..." );
-    await ComfyDB.Init();
-    console.log( "creating collection..." );
-    await ComfyDB.Collections.Create( "test" );
-    console.log( "listing collections..." );
-    var collections = await ComfyDB.Collections.List();
-    console.log( collections );
-    await ComfyDB.Data.SetByKey( "test", "a", { name: "test" }, true );
-    var item = await ComfyDB.Data.FindByKey( "test", "a" );
-    console.log( item );
-    await new Promise((res) => setTimeout(res, 1000));
-    await ComfyDB.Data.SetByKey( "test", [ "a", "b", "c" ], [ { name: "A", points: 5 }, { name: "B" }, { name: "C" } ], true );
-    await ComfyDB.Data.SetByKey( "test", "a", { name: "Z" }, true );
-    var all = await ComfyDB.Data.FindLatest( "test" );//await ComfyDB.Data.Find( "test", {} );
-    console.table( all );
-    await ComfyDB.Data.Increment( "test", [ "a", "b" ], "points", 10 );
-    var itemA = await ComfyDB.Data.FindByField( "test", "name", ComfyDB.Is.NotEqual, "C" );
-    console.table( itemA );
-    await ComfyDB.Data.Decrement( "test", [ "a", "b" ], "points", 3 );
-    var itemA = await ComfyDB.Data.FindByField( "test", "name", ComfyDB.Is.NotEqual, "C" );
-    console.table( itemA );
-    await ComfyDB.Data.DeleteByKey( "test", "b" );
-    all = await ComfyDB.Data.FindLatest( "test" );//await ComfyDB.Data.Find( "test", {} );
-    console.table( all );
-    console.log( "deleting collection..." );
-    await ComfyDB.Collections.Delete( "test" );
-    console.log( "listing collections..." );
-    collections = await ComfyDB.Collections.List();
-    console.log( collections );
+    console.log( "connecting..." );
+	await ComfyDB.Connect();
+	console.log( "Is Connected:", ComfyDB.IsConnected() );
+
+	console.log( "Get:", await ComfyDB.Get( "time" ) );
+	console.log( "Store:", await ComfyDB.Store( "time", { date: new Date() } ) );
+	console.log( "Store:", await ComfyDB.Store( "name", { name: "instafluff" } ) );
+	console.log( "Get:", await ComfyDB.Get( "time" ) );
+	console.log( "Search:", await ComfyDB.Search( {} ) );
+
+	// console.table( all );
+	//
+	// ComfyDB.Store( "key", [{ stuff }], group = "ComfyDB" );
+	// ComfyDB.Get( "key", options = null, group = "ComfyDB" );
+	// - ComfyDB.Delete( "key", options = null, group = "ComfyDB" );
+	// ComfyDB.Increment( "key", { options } = null, group = "ComfyDB" );
+	// ComfyDB.Decrement( "key", { options } = null, group = "ComfyDB" );
+	// ComfyDB.Count( "key", { options } = null, group = "ComfyDB" );
+	ComfyDB.Close();
   }
   catch( ex ) {
     console.log( ex );
@@ -41,7 +31,18 @@ async function testComfy() {
   }
 }
 
-// (async () => {
-//   testComfy();
-// })();
-testComfy();
+
+const ComfyMongo = require( "comfy-mongo" )();
+ComfyMongo.on( "output", ( data ) => {
+  // console.log( data );
+});
+ComfyMongo.on( "error", ( err ) => {
+  // console.log( err );
+});
+ComfyMongo.on( "ready", async () => {
+  console.log( "[ComfyDB] Ready..." );
+  testComfy();
+});
+ComfyMongo.on( "exit", ( code ) => {
+  console.log( "[ComfyDB] Exit:", code );
+});
